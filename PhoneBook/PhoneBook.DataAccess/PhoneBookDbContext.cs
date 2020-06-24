2388af6b6f5974653c1081fa26a9430e29a0e06a
Roman Gubkin <romangubkin1995@gmail.com>
2020-06-24 21:25:28 +0700

    Started PhoneBook

---
+++ PhoneBook/PhoneBook.DataAccess/PhoneBookDbContext.cs
@@ -0,0 +1,19 @@
+﻿using PhoneBook.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.DataAccess
+{
+    public class PhoneBookDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public PhoneBookDbContext() : base("PhoneBookConnection")
+        {
+
+        }
+    }
+}
